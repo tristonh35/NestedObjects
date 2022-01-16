e5bede19cd63bbfa9d6e62f5f572fd55c121f4bb
tristonh35 <tristonhall35@yahoo.com>
2022-01-16 00:27:17 -0800

    Created student and advisor classes

---
+++ Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#nullable disable
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents a individual college student
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// The legal first name
+        /// </summary>
+        public string FirstName { get; set; }   
+
+        /// <summary>
+        /// The legal last name
+        /// </summary>
+        public string LastName { get; set; } 
+
+        /// <summary>
+        /// The assigned school email address
+        /// </summary>
+        public string SchoolEmail { get; set; }
+
+        /// <summary>
+        /// The primary phone number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        public Advisor AssignedAdvisor { get; set; }    
+    }
+}
